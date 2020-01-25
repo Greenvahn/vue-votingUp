7069d8faaa00e3f1248a03e48222a5f030840633
Saul Verde <saulverdemartinez@gmail.com>
2020-01-25 12:37:17 +0000

    Added first components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title msg="Vote the best articles!"></Title>
+    <Article></Article>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+import Article from './components/Article.vue'
+import {data} from './js/data.js'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Title,
+    Article
+  },
+  data(){
+    return{
+      stateData: data.articles
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="scss">
+   @import 'bulma';
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div class="container">
+    <h2 class="title has-text-centered dividing-header is-1">{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'title',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+ 
